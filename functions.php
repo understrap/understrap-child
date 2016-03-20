295b7e078d280f992c5becfc7c754a7bd1de0468
Holger Könemann <office@holgerkoenemann.de>
2016-03-20 14:18:22 +0100

    streamline npm/bower/sass workflow between parent and child theme

--- functions.php
+++ functions.php
@@ -2,4 +2,5 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'understrap-theme', get_stylesheet_directory_uri() . '/css/understrap-child.min.css', array());
+     wp_enqueue_script( 'understrap-main', get_stylesheet_directory_uri() . '/js/theme.min.js', array(), '0.1.0', true );
 }
