e0f56f30dc50cd62b89e1102d41bbecd8d1fb9c7
Holger Könemann <office@holgerkoenemann.de>
2016-04-23 11:24:03 +0200

    Deregister parent theme js files

--- functions.php
+++ functions.php
@@ -3,12 +3,15 @@ function understrap_remove_scripts() {
     wp_dequeue_style( 'understrap-styles' );
     wp_deregister_style( 'understrap-styles' );
 
-    // Removes the parent themes stylesheet from inc/enqueue.php
+    wp_dequeue_style( 'understrap-scripts' );
+    wp_deregister_style( 'understrap-scripts' );
+
+    // Removes the parent themes stylesheet and scripts from inc/enqueue.php
 }
 add_action( 'wp_enqueue_scripts', 'understrap_remove_scripts', 20 );
 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
-    wp_enqueue_style( 'understrap-theme', get_stylesheet_directory_uri() . '/css/understrap-child.min.css', array());
-     wp_enqueue_script( 'understrap-main', get_stylesheet_directory_uri() . '/js/theme.min.js', array(), '0.1.0', true );
+    wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/understrap-child.min.css', array());
+    wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/theme.min.js', array(), '0.1.0', true );
 }
