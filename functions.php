09b9392f066b5b88736aa09456845c1174808cde
Holger Könemann <office@holgerkoenemann.de>
2016-04-23 11:21:16 +0200

    unregister missing parent themes css thx to @sandilands
    The child theme loads it´s own css file and don´t need the parent
    themes css. But it uses the parent themes inc/enqueue.php file which
    register and enqueued the parent themes css file from the child themes
    directory. Caused a 404 request. Fixed with this changes.

--- functions.php
+++ functions.php
@@ -1,4 +1,12 @@
 <?php
+function understrap_remove_scripts() {
+    wp_dequeue_style( 'understrap-styles' );
+    wp_deregister_style( 'understrap-styles' );
+
+    // Removes the parent themes stylesheet from inc/enqueue.php
+}
+add_action( 'wp_enqueue_scripts', 'understrap_remove_scripts', 20 );
+
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'understrap-theme', get_stylesheet_directory_uri() . '/css/understrap-child.min.css', array());
