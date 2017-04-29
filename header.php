446f18b67b9f968cf4ccdbd59eac2d73e9d6054e
David Griffin <dave@digidol-media.net>
2017-04-29 23:23:58 +0100

    White brand-nav

--- header.php
+++ header.php
@@ -33,7 +33,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 		<a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e( 'Skip to content',
 		'understrap' ); ?></a>
 
-		<nav class="navbar navbar-toggleable-md fixed-top  transparent">
+		<nav class="navbar navbar-toggleable-md fixed-top transparent">
 
 		<?php if ( 'container' == $container ) : ?>
 			<div class="container">
