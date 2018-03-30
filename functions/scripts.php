a1e89f241a2d7cae843da51e1fc6cab8a3a95efb
Elliot <elliot@raison.co>
2018-03-30 15:29:52 +0100

    Debug Enqueue

--- functions/scripts.php
+++ functions/scripts.php
@@ -1,4 +1,14 @@
 <?php
+
+// set debug version
+if (true == WP_DEBUG) {
+    $date = new DateTime();
+    $ver_num = $date->getTimestamp();
+} else {
+    $ver_num = wp_get_theme()->get( 'Version' );
+}
+
+
 function understrap_remove_scripts() {
     wp_dequeue_style( 'understrap-styles' );
     wp_deregister_style( 'understrap-styles' );
@@ -8,18 +18,23 @@ function understrap_remove_scripts() {
 
     // Removes the parent themes stylesheet and scripts from inc/enqueue.php
 }
+
 add_action( 'wp_enqueue_scripts', 'understrap_remove_scripts', 20 );
 
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
 
-	// Get the theme data
-	$the_theme = wp_get_theme();
-    wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $the_theme->get( 'Version' ) );
-    wp_enqueue_script( 'jquery');
-	wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), false);
-    wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
-    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-        wp_enqueue_script( 'comment-reply' );
+    // if debug load non-min css
+    if (true == WP_DEBUG) {
+        wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.css', array(), $ver_num);
+        wp_enqueue_script( 'child-scripts', get_stylesheet_directory_uri() . '/js/child-theme.js', array(), $ver_num, true );
+    } else {
+        wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $ver_num);
+        wp_enqueue_script( 'child-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $ver_num, true );
     }
+
+    wp_enqueue_script( 'child-scripts', get_stylesheet_directory_uri() . '/js/theme.js', array(), $ver_num, true );
+
+
 }
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
