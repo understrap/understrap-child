d86ee1d26c8d8f125addf9ba7c8c47ad8f1f32df
Ken Moini <ken@kenmoini.com>
2018-11-24 19:36:53 -0600

    Update functions.php to fix popper.js loading
    Child themes return the parent directory for get_theme_directory_uri...get_stylesheet_directory_uri fixes this.

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ function theme_enqueue_styles() {
 	$the_theme = wp_get_theme();
     wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $the_theme->get( 'Version' ) );
     wp_enqueue_script( 'jquery');
-	wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), false);
+	wp_enqueue_script( 'popper-scripts', get_stylesheet_directory_uri() . '/js/popper.min.js', array(), false);
     wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
