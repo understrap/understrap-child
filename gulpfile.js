8142bca284c1209ca2ca68c6b6304fd3a017f41a
Ken Eucker <keneucker@gmail.com>
2019-04-08 23:11:43 -0700

    Converted string concatenation to template literals.

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ var paths = cfg.paths;
 // gulp sass
 // Compiles SCSS files in CSS
 gulp.task( 'sass', function() {
-    var stream = gulp.src( paths.sass + '/*.scss' )
+    var stream = gulp.src( `${paths.sass}/*.scss` )
         .pipe( sourcemaps.init( { loadMaps: true } ) )
         .pipe( plumber( {
             errorHandler: function( err ) {
@@ -44,18 +44,18 @@ gulp.task( 'sass', function() {
 // gulp watch
 // Starts watcher. Watcher runs gulp sass task on changes
 gulp.task( 'watch', function() {
-    gulp.watch( paths.sass + '/**/*.scss', gulp.series('styles') );
-    gulp.watch( [paths.dev + '/js/**/*.js', 'js/**/*.js', '!js/child-theme.js', '!js/child-theme.min.js'], gulp.series('scripts') );
+    gulp.watch( `${paths.sass}/**/*.scss`, gulp.series('styles') );
+    gulp.watch( [`${paths.dev}/js/**/*.js`, 'js/**/*.js', '!js/child-theme.js', '!js/child-theme.min.js'], gulp.series('scripts') );
 
     //Inside the watch task.
-    gulp.watch( paths.imgsrc + '/**', gulp.series('imagemin-watch') );
+    gulp.watch( `${paths.imgsrc} /**`, gulp.series('imagemin-watch') );
 });
 
 // Run:
 // gulp imagemin
 // Running image optimizing task
 gulp.task( 'imagemin', function() {
-    gulp.src( paths.imgsrc + '/**' )
+    gulp.src( `${paths.imgsrc}/**` )
     .pipe( imagemin() )
     .pipe( gulp.dest( paths.img ) );
 });
@@ -72,7 +72,7 @@ gulp.task( 'imagemin-watch', gulp.series('imagemin', function reloadBrowserSync(
 // gulp cssnano
 // Minifies CSS files
 gulp.task( 'cssnano', function() {
-  return gulp.src( paths.css + '/child-theme.css' )
+  return gulp.src( `${paths.css}/child-theme.css` )
     .pipe( sourcemaps.init( { loadMaps: true } ) )
     .pipe( plumber( {
             errorHandler: function( err ) {
@@ -87,7 +87,7 @@ gulp.task( 'cssnano', function() {
 });
 
 gulp.task( 'minifycss', function() {
-  return gulp.src( paths.css + '/child-theme.css' )
+  return gulp.src( `${paths.css}/child-theme.css}`, { allowEmpty: true } )
   .pipe( sourcemaps.init( { loadMaps: true } ) )
     .pipe( cleanCSS( { compatibility: '*' } ) )
     .pipe( plumber( {
@@ -102,7 +102,7 @@ gulp.task( 'minifycss', function() {
 });
 
 gulp.task( 'cleancss', function() {
-  return gulp.src( paths.css + '/*.min.css', { read: false } ) // Much faster
+  return gulp.src( `${paths.css}/*.min.css`, { read: false } ) // Much faster
     .pipe( ignore( 'child-theme.css' ) )
     .pipe( rimraf() );
 });
@@ -123,15 +123,15 @@ gulp.task( 'scripts', function() {
     var scripts = [
 
         // Start - All BS4 stuff
-        paths.dev + '/js/bootstrap4/bootstrap.bundle.js',
+        `${paths.dev}/js/bootstrap4/bootstrap.bundle.js`,
 
         // End - All BS4 stuff
 
-        paths.dev + '/js/skip-link-focus-fix.js',
+        `${paths.dev}/js/skip-link-focus-fix.js`,
 
         // Adding currently empty javascript file to add on for your own themes´ customizations
         // Please add any customizations to this .js file only!
-        paths.dev + '/js/custom-javascript.js'
+        `${paths.dev}js/custom-javascript.js`,
     ];
   gulp.src( scripts, { allowEmpty: true } )
     .pipe( concat( 'child-theme.min.js' ) )
@@ -150,7 +150,7 @@ gulp.task( 'watch-bs', gulp.parallel('browser-sync', 'watch', 'scripts'));
 
 // Deleting any file inside the /src folder
 gulp.task('clean-source', function () {
-  return del(['src/**/*',]);
+  return del(['src/**/*']);
 });
 
 // Run:
@@ -162,76 +162,81 @@ gulp.task( 'copy-assets', function() {
 
 ////////////////// All Bootstrap 4 Assets /////////////////////////
 // Copy all JS files
-    var stream = gulp.src( paths.node + 'bootstrap/dist/js/**/*.js' )
-        .pipe( gulp.dest( paths.dev + '/js/bootstrap4' ) );
+    var stream = gulp.src( `${paths.node}bootstrap/dist/js/**/*.js` )
+        .pipe( gulp.dest( `${paths.dev}/js/bootstrap4` ) );
 
 // Copy all Bootstrap SCSS files
-    gulp.src( paths.node + 'bootstrap/scss/**/*.scss' )
-        .pipe( gulp.dest( paths.dev + '/sass/bootstrap4' ) );
+    gulp.src( `${paths.node}bootstrap/scss/**/*.scss` )
+        .pipe( gulp.dest( `${paths.dev}/sass/bootstrap4` ) );
 
 ////////////////// End Bootstrap 4 Assets /////////////////////////
 
 // Copy all Font Awesome Fonts
-    gulp.src( paths.node + 'font-awesome/fonts/**/*.{ttf,woff,woff2,eot,svg}' )
+    gulp.src( `${paths.node}font-awesome/fonts/**/*.{ttf,woff,woff2,eot,svg}` )
         .pipe( gulp.dest( './fonts' ) );
 
 // Copy all Font Awesome SCSS files
-    gulp.src( paths.node + 'font-awesome/scss/*.scss' )
-        .pipe( gulp.dest( paths.dev + '/sass/fontawesome' ) );
+    gulp.src( `${paths.node}font-awesome/scss/*.scss` )
+        .pipe( gulp.dest( `${paths.dev}/sass/fontawesome` ) );
 
 
 // _s SCSS files
-    gulp.src( paths.node + 'undescores-for-npm/sass/media/*.scss' )
-        .pipe( gulp.dest( paths.dev + '/sass/underscores' ) );
+    gulp.src( `${paths.node}undescores-for-npm/sass/media/*.scss` )
+        .pipe( gulp.dest( `${paths.dev}/sass/underscores` ) );
 
 // _s JS files into /src/js
-    gulp.src( paths.node + 'undescores-for-npm/js/skip-link-focus-fix.js' )
-        .pipe( gulp.dest( paths.dev + '/js' ) );
+    gulp.src( `${paths.node}undescores-for-npm/js/skip-link-focus-fix.js` )
+        .pipe( gulp.dest( `${paths.dev}/js` ) );
 
 // Copy Popper JS files
-    gulp.src( paths.node + 'popper.js/dist/umd/popper.min.js' )
-        .pipe( gulp.dest( paths.js + paths.vendor ) );
-    gulp.src( paths.node + 'popper.js/dist/umd/popper.js' )
-        .pipe( gulp.dest( paths.js + paths.vendor ) );
+    gulp.src( `${paths.node}popper.js/dist/umd/popper.min.js` )
+        .pipe( gulp.dest( `${paths.js}${paths.vendor}` ) );
+    gulp.src( `${paths.node}popper.js/dist/umd/popper.js` )
+        .pipe( gulp.dest( `${paths.js}${paths.vendor}` ) );
 
 // UnderStrap SCSS files
-    gulp.src( paths.node + 'understrap/sass/**/*.scss' )
-        .pipe( gulp.dest( paths.dev + '/sass/understrap' ) );
+    gulp.src( `${paths.node}understrap/sass/**/*.scss` )
+        .pipe( gulp.dest( `${paths.dev}/sass/understrap` ) );
 
     return stream;
 });
 
 // Deleting the files distributed by the copy-assets task
 gulp.task( 'clean-vendor-assets', function() {
-  return del( [paths.dev + '/js/bootstrap4/**', paths.dev + '/sass/bootstrap4/**', './fonts/*wesome*.{ttf,woff,woff2,eot,svg}', paths.dev + '/sass/fontawesome/**', paths.dev + '/sass/underscores/**', paths.dev + '/js/skip-link-focus-fix.js', paths.js + '/**/skip-link-focus-fix.js', paths.js + '/**/popper.min.js', paths.js + '/**/popper.js', ( paths.vendor !== ''?( paths.js + paths.vendor + '/**' ):'' )] );
+  return del( [`${paths.dev}/js/bootstrap4/**`, `${paths.dev}/sass/bootstrap4/**`, './fonts/*wesome*.{ttf,woff,woff2,eot,svg}', `${paths.dev}/sass/fontawesome/**`, `${paths.dev}/sass/underscores/**`, `${paths.dev}/js/skip-link-focus-fix.js`, `${paths.js}/**/skip-link-focus-fix.js`, `${paths.js}/**/popper.min.js`, `${paths.js}/**/popper.js`, ( paths.vendor !== ''?( `${paths.js}${paths.vendor}/**` ):'' )] );
 });
 
 // Deleting any file inside the /dist folder
 gulp.task( 'clean-dist', function() {
-  return del( [paths.dist + '/**'] );
+  return del( [`${paths.dist}/**`] );
 });
 
 // Run
 // gulp dist
 // Copies the files to the /dist folder for distribution as simple theme
 gulp.task( 'dist', gulp.series('clean-dist', function copyToDistFolder() {
-  return gulp.src( ['**/*', '!' + paths.bower, '!' + paths.bower + '/**', '!' + paths.node, '!' + paths.node + '/**', '!' + paths.dev, '!' + paths.dev + '/**', '!' + paths.dist, '!' + paths.dist + '/**', '!' + paths.distprod, '!' + paths.distprod + '/**', '!' + paths.sass, '!' + paths.sass + '/**', '!readme.txt', '!readme.md', '!package.json', '!package-lock.json', '!gulpfile.js', '!gulpconfig.json', '!CHANGELOG.md', '!.travis.yml', '!jshintignore',  '!codesniffer.ruleset.xml',  '*'], { 'buffer': false } )
-  .pipe( replace( '/js/jquery.slim.min.js', '/js' + paths.vendor + '/jquery.slim.min.js', { 'skipBinary': true } ) )
-  .pipe( replace( '/js/popper.min.js', '/js' + paths.vendor + '/popper.min.js', { 'skipBinary': true } ) )
-  .pipe( replace( '/js/skip-link-focus-fix.js', '/js' + paths.vendor + '/skip-link-focus-fix.js', { 'skipBinary': true } ) )
+    const ignorePaths = [`!${paths.bower}`, `!${paths.bower}/**`, `!${paths.node}`, `!${paths.node}/**`, `!${paths.dev}`, `!${paths.dev}/**`, `!${paths.dist}`, `!${paths.dist}/**`, `!${paths.distprod}`, `!${paths.distprod}/**`, `!${paths.sass}`, `!${paths.sass}/**`],
+    ignoreFiles = [ '!readme.txt', '!readme.md', '!package.json', '!package-lock.json', '!gulpfile.js', '!gulpconfig.json', '!CHANGELOG.md', '!.travis.yml', '!jshintignore',  '!codesniffer.ruleset.xml' ];
+
+    console.log({ ignorePaths, ignoreFiles })
+    
+  return gulp.src( ['**/*', ...ignorePaths, ...ignoreFiles,  '*'], { 'buffer': false } )
+  .pipe( replace( '/js/jquery.slim.min.js', `/js${paths.vendor}/jquery.slim.min.js`, { 'skipBinary': true } ) )
+  .pipe( replace( '/js/popper.min.js', `/js${paths.vendor}/popper.min.js`, { 'skipBinary': true } ) )
+  .pipe( replace( '/js/skip-link-focus-fix.js', `/js${paths.vendor}/skip-link-focus-fix.js`, { 'skipBinary': true } ) )
     .pipe( gulp.dest( paths.dist ) );
 }));
 
 // Deleting any file inside the /dist-product folder
 gulp.task( 'clean-dist-product', function() {
-  return del( [paths.distprod + '/**'] );
+  return del( [`${paths.distprod}/**`] );
 } );
 
 // Run
 // gulp dist-product
 // Copies the files to the /dist-prod folder for distribution as theme with all assets
 gulp.task( 'dist-product', gulp.series('clean-dist-product', function copyToDistFolder() {
-  return gulp.src( ['**/*', '!' + paths.bower, '!' + paths.bower + '/**', '!' + paths.node, '!' + paths.node + '/**', '!' + paths.dist, '!' + paths.dist +'/**', '!' + paths.distprod, '!' + paths.distprod + '/**', '*'] )
+  return gulp.src( ['**/*', `!${paths.bower}`, `!${paths.bower}/**`, `!${paths.node}`, `!${paths.node}/**`, `!${paths.dist}`, `!${paths.dist}` +'/**', `!${paths.distprod}`, `!${paths.distprod}/**`, '*'] )
     .pipe( gulp.dest( paths.distprod ) );
 } ));
 
