cba501d04da932c7b44db6cfc2957a7f2bfc8a3f
Brian Coords <bacoords@gmail.com>
2021-08-23 21:23:38 -0700

    updates readme for new build process

--- js/child-theme.js
+++ js/child-theme.js
@@ -1,6 +1,6 @@
 /*!
   * Understrap v0.6.0 (https://understrap.com)
-  * Copyright 2013-2021 The UnderStrap Authors (https://github.com/understrap/understrap/graphs/contributors)
+  * Copyright 2013-2021 The Understrap Authors (https://github.com/understrap/understrap/graphs/contributors)
   * Licensed under GPL (http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
   */
 (function (global, factory) {
